bb53a6f1735ce833d728e81ed8ea8f0295551cb6
Dell'aiera florian <floriandellaiera@gmail.com>
2014-11-18 15:29:28 +0900

    threats Xss done

---
+++ www/Xss.php
@@ -0,0 +1,9 @@
+<?php 
+	echo $_POST['simpleAttack'];
+	echo $_GET['LastName'] . " ". $_GET["FirstName"];
+	echo $_POST['value'];
+
+	echo htmlspecialchars($_POST["CorrectionsimpleAttack"]);
+	echo htmlspecialchars($_GET["Correction"]);
+	echo htmlspecialchars($_POST["CorrectionValue"]);
+?> 
