ff2157db0d73217d78888adb4303af4aaa695e46
Alvarocau <alvarofernandezdiaz@MacBook-Pro-de-Alvaro-Fernandez-Diaz.local>
2014-11-30 02:56:18 +0900

    Sensitive data exposure

---
+++ www/payment3.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<p>Your transanction is not valid, please try again later or diferent credit card</p>
+<p>&nbsp;</p>
+<p>-----------------------------------------------------------------------</p>
+<p>&nbsp;</p>
+<p>Sensitive data Exposure sent to hacker e-mail</p>
+<FORM method=post action= action="Payment.php">
+  <TABLE BORDER=0>
+<TR>
+	<TD>CARD</TD>
+	<TD>
+	1234567890123456
+	</TD>
+</TR>
+
+<TR>
+	<TD>Expiration</TD>
+	<TD>
+      <INPUT type=text name="mm" value="11" size="6" maxlength="2" />
+      <INPUT type=text name="yy" value="22" size="6" maxlength="2">
+</TR>
+<TR>
+	<TD>CVV</TD>
+	<TD>
+      <INPUT type=text name="CVV" value="123" size="8" maxlength="3">
+</TR>
+
+<TR>
+  <TD COLSPAN=2>
+    ALVARO FERNANDEZ VISA CARD</TD>
+</TR>
+</TABLE>
+</FORM>
+</body>
+</html>
